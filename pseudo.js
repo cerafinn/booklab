a27605357642aaaa6d5ba4babd29db6e1ac83955
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-09-14 19:27:36 -0400

    set up initial files for project

---
+++ pseudo.js
@@ -0,0 +1,8 @@
+// create a backend
+// with user keys and an admin
+
+
+// create frontend where:
+// the admin view will see who clocked in and out of the lab
+// the users can clock in/out of the lab, can see their own clock times, get reminders checking if they are still at the lab
+// time sheets for users and admin, which will count up in time for the week, along with a total for the week
